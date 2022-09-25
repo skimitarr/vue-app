f668456b0a2b59122a63847b7eef58e5f90fa5d4
unknown <grisha.lakoza@gmail.com>
2022-09-25 19:25:19 +0300

    problem

--- src/App.vue
+++ src/App.vue
@@ -1,86 +1,35 @@
 <template>
   <section class="catalog">
     <ProductList v-bind:products="products"></ProductList>
-    <!-- <ul class="catalog__list">
-      <li class="catalog__item" v-for='(product, index) in products' :key='index'>
-        <a class="catalog__pic" href="#">
-          <img v-bind:src="product.image" v-bind:alt="product.title">
-        </a>
+    <BasePagination
+      v-model:page="page" v-bind:count="countProducts" :per-page="productsPerPage">
+    </BasePagination>
 
-        <h3 class="catalog__title">
-          <a href="#">
-            {{ product.title }}
-          </a>
-        </h3>
-
-        <span class="catalog__price">
-          {{ product.price }} p
-        </span>
-
-      </li>
-    </ul> -->
-
-    <ul class="catalog__pagination pagination">
-      <li class="pagination__item">
-        <a class="pagination__link pagination__link--arrow pagination__link--disabled"
-        aria-label="Предыдущая страница">
-          <svg width="8" height="14" fill="currentColor">
-            <use xlink:href="#icon-arrow-left"></use>
-          </svg>
-        </a>
-      </li>
-      <li class="pagination__item">
-        <a class="pagination__link pagination__link--current">
-          1
-        </a>
-      </li>
-      <li class="pagination__item">
-        <a class="pagination__link" href="#">
-          2
-        </a>
-      </li>
-      <li class="pagination__item">
-        <a class="pagination__link" href="#">
-          3
-        </a>
-      </li>
-      <li class="pagination__item">
-        <a class="pagination__link" href="#">
-          4
-        </a>
-      </li>
-      <li class="pagination__item">
-        <a class="pagination__link" href="#">
-          ...
-        </a>
-      </li>
-      <li class="pagination__item">
-        <a class="pagination__link" href="#">
-          10
-        </a>
-      </li>
-      <li class="pagination__item">
-        <a class="pagination__link pagination__link--arrow" href="#" aria-label="Следующаястраница">
-          <svg width="8" height="14" fill="currentColor">
-            <use xlink:href="#icon-arrow-right"></use>
-          </svg>
-        </a>
-      </li>
-    </ul>
   </section>
 </template>
 
 <script>
 import products from './data/products';
 import ProductList from './components/ProductList.vue';
+import BasePagination from './components/BasePagination.vue';
 
 export default {
   name: 'App',
-  components: { ProductList },
+  components: { ProductList, BasePagination },
   data() {
     return {
-      products,
+      page: 1,
+      productsPerPage: 3,
     };
+  },
+  computed: {
+    products() {
+      const offset = (this.page - 1) * this.productsPerPage;
+      return products.slice(offset, offset + this.productsPerPage);
+    },
+    countProducts() {
+      return products.length;
+    },
   },
 };
 </script>
